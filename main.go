6266f734a2abe133805d5d141be6ca745747a116
I Komang Sugiartha <sugiartha26@gmail.com>
2020-10-17 20:48:12 +0700

    repo

--- main.go
+++ main.go
@@ -1,31 +1,41 @@
 package main
 
 import (
+	"apigolangmysql/mahasiswa"
+	"apigolangmysql/utils"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/kodingin/api-mysql/config"
 )
 
 func main() {
 
-	db, e := config.MySQL()
-
-	if e != nil {
-		log.Fatal(e)
-	}
-
-	eb := db.Ping()
-	if eb != nil {
-		panic(eb.Error())
-	}
-
-	fmt.Println("Success")
-
+	http.HandleFunc("/mahasiswa", GetMahasiswa)
 	err := http.ListenAndServe(":7000", nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// GetMahasiswa
+func GetMahasiswa(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		defer cancel()
+
+		mahasiswas, err := mahasiswa.GetAll(ctx)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		utils.ResponseJSON(w, mahasiswas, http.StatusOK)
+		return
+	}
+
+	http.Error(w, "Tidak di ijinkan", http.StatusNotFound)
+	return
 }
